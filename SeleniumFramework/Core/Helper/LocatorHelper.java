77a042436d6ba674e0e54140c512ac18607b9f75
tuan.m.ngo <tuanngo1764@gmail.com>
2022-10-05 00:44:50 +0700

    Update Core Element

--- SeleniumFramework/Core/Helper/LocatorHelper.java
+++ SeleniumFramework/Core/Helper/LocatorHelper.java
@@ -5,18 +5,24 @@ import org.javatuples.Pair;
 import ElementSetting.FindBy;
 
 public class LocatorHelper {
-	
+
 	public static String getLocatorType(String locator) {
 		return locator.replaceAll("([\\w\\s]*)=.*", "$1").trim();
 	}
-	
+
 	public static String getLocatorValue(String locator) {
 		return locator.replaceAll("[\\w\\s]*=(.*)", "$1").trim();
 	}
 
 	public static Pair<FindBy, String> getPairLocator(String locator) {
 		FindBy locatorType = FindBy.getByLocator(getLocatorType(locator));
-		String locatorValue = getLocatorValue(locator);
+		String locatorValue = "";
+
+		if (locatorType == FindBy.XPATH && !locator.contains(FindBy.XPATH.getValue()))
+			locatorValue = getLocatorValue(String.format("%s=%s", FindBy.XPATH.getValue(), locator));
+		else {
+			locatorValue = getLocatorValue(locator);
+		}
 		return new Pair<FindBy, String>(locatorType, locatorValue);
 	}
 }
