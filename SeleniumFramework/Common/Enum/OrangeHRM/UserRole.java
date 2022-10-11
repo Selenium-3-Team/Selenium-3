8ca50471baafd6454bc097a9ecd03053effd05c9
tuan.m.ngo <tuanngo1764@gmail.com>
2022-10-11 20:36:15 +0700

    Update tests

---
+++ SeleniumFramework/Common/Enum/OrangeHRM/UserRole.java
@@ -0,0 +1,25 @@
+package Enum.OrangeHRM;
+
+public enum UserRole {
+	/**
+	 * Declare constants for user role
+	 */
+	ADMIN("admin"), ESS("ess");
+
+	private final String userRole;
+
+	UserRole(final String userRole) {
+		this.userRole = userRole;
+	}
+
+	/**
+	 * 
+	 * Gets the user role.
+	 * 
+	 * @return String, the user role
+	 */
+	public String getUserRole() {
+		return this.userRole;
+	}
+
+}
