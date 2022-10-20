3c91834315b591880fc3da7815dd8e800583024a
tuan.m.ngo <tuanngo1764@gmail.com>
2022-10-20 14:16:52 +0700

    Update

--- SeleniumFramework/src/test/java/tests/OrangeHRM/AdminTest.java
+++ SeleniumFramework/src/test/java/tests/OrangeHRM/AdminTest.java
@@ -8,6 +8,7 @@ import core.report.Logger;
 import dataType.OrangeHRM.Account;
 import dataType.OrangeHRM.TopBarMenuItem;
 import dataType.OrangeHRM.UserRole;
+import io.qameta.allure.Description;
 import pages.OrangeHRM.AdminPage;
 import pages.OrangeHRM.HomePage;
 import pages.OrangeHRM.LoginPage;
@@ -22,8 +23,8 @@ public class AdminTest extends TestBase {
 	AdminPage adminPage = AdminPage.newInstance();
 
 	@Test
+	@Description("Test case 05: Admin User can search employees successfully by \"User Role\".")
 	public void TC05() {
-		Logger.info("Test case 05: Admin User can search employees successfully by \"User Role\"");
 
 		Account account = new Account(UserRole.ADMIN);
 
